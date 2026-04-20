bc876da096a7930c69ba4bda8859c61474f7ddb4
Siri759 <sdadmom759@gmail.com>
2026-04-20 16:35:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,20 @@ question = st.text_input("Ask your question (enter scheme name)")
 
 if question:
     question_lower = question.lower()
-    matches = []
+    matches = [def calculate_score(fund):
+    score = 0
+    
+    if fund["risk"] == "Low":
+        score += 3
+    elif fund["risk"] == "Moderate":
+        score += 2
+    else:
+        score += 1
+
+    if fund["category"] == "Equity":
+        score += 2
+
+    return score]
 
     # Smart long-term recommendation logic
     long_term_keywords = ["long term", "long-term", "safe", "best"]
